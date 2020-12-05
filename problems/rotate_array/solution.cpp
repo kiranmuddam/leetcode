ee765776dde3d60b81b6f93316d0bc2ca057cf2a
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-12-05 03:07:02 +0000

    Sync LeetCode submission - Rotate Array (cpp)

---
+++ problems/rotate_array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (nums.size() < 2) {
+            return;
+        }
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+    
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
