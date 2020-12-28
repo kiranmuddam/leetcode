570f702e3e3fb9e8d6caec3fdd150d4e33c5f289
Kiran Babu Muddam <kiraniiitn@gmail.com>
2020-12-28 02:44:56 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- problems/reverse_integer/solution.cpp
+++ problems/reverse_integer/solution.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-        long long int reverse(long long int x) {
-        long long int reverseDigit=0;
-        long long int temp=abs(x);
-        while(temp>0){
-            reverseDigit=(reverseDigit*10)+(temp%10);
-            temp=temp/10;
-        }
-        if((reverseDigit>2147483647) or (reverseDigit< -2147483648)){
+    int reverse(int x) {
+        int envMax = INT_MAX;
+        int envMin = INT_MIN;
+        bool isNegative = false;
+        long int reversedNumber = 0;
+        if (x == 0 || x == INT_MAX || x == INT_MIN) {
             return 0;
         }
-        else if(x<0){
-            reverseDigit=reverseDigit*(-1);
+        if (x < 0) {
+            isNegative = true;
+            x *= -1;
+        }
+        int count = floor(log10(x) + 1);
+        while (x > 0) {
+            reversedNumber += (x % 10) * pow(10, count - 1);
+            x = x / 10;
+            count--;
         }
         
-        return reverseDigit;    
+        if (isNegative) {
+            reversedNumber *= -1;
+        }
+        
+        if (reversedNumber < envMin || reversedNumber > envMax) {
+            return 0;
+        } else {
+            return reversedNumber;
+        }
+        
     }
 };
